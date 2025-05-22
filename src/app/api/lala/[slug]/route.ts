fea210527ce1342d91367b13bfc950e39cc34006
Sadiq Naqvi <naqvisadiq6@gmail.com>
2025-05-23 03:28:33 +0530

    Fixed missing stuff in generate route

--- src/app/api/lala/[slug]/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const GET = (req: any, { params }: { params: { slug: string } }) => {
-  console.log(params.slug);
-  return NextResponse.json({ success: true });
-};
